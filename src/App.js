55792eee36c62172de323afb3d1d9b6bbd8d8e74
Duarte Almeida <duartemalmeida@gmail.com>
2021-10-02 16:58:44 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Start from "./components/Start";
+import Game from "./components/Game";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Start}></Route>
+      </Switch>
+      <Switch>
+        <Route path="/game" component={Game}></Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
