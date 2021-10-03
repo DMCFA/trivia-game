9a420f5b20345af117792948ed78e27dd4de628c
Duarte Almeida <duartemalmeida@gmail.com>
2021-10-03 16:02:26 +0100

    refactor and export some logic to begin component

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,25 +1,18 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import { useHistory } from "react-router";
-import Theme from "./Theme";
-import Difficulty from "./Difficulty";
-import Amount from "./Amount";
 
 const Start = () => {
   const history = useHistory();
   const handleStart = () => {
     history.push("/game");
   };
-
   return (
-    <div className="start-main">
-      <Theme />
-      <Difficulty />
-      <Amount />
-      <div className="begin">
-        <h1 className="begin-title">Trivia Game</h1>
+    <div className="start">
+      <h2 className="start-title">Ready to Go?</h2>
+      <div className="start-btn-group">
         <Button
-          className="begin-btn"
+          className="start-btn"
           size="lg"
           variant="success"
           onClick={handleStart}
