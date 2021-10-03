5eb8a5a40f9c519e00be587c4004a0c32dcc8054
Duarte Almeida <duartemalmeida@gmail.com>
2021-10-03 12:33:22 +0100

    add button group

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,12 +1,46 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, ButtonGroup } from "react-bootstrap";
 
 const Theme = () => {
   const [theme, setTheme] = useState("");
   return (
     <div className="theme">
       <h2 className="theme-name">Choose your theme</h2>
-      <Button></Button>
+      <div className="theme-btn-group">
+        <ButtonGroup aria-label="group of buttons" size="lg">
+          <Button className="theme-btn" onClick={() => setTheme("books")}>
+            Books
+          </Button>
+          <Button
+            className="theme-btn"
+            variant="success"
+            onClick={() => setTheme("movies")}
+          >
+            Movies
+          </Button>
+          <Button
+            className="theme-btn"
+            variant="warning"
+            onClick={() => setTheme("tv")}
+          >
+            TV Shows
+          </Button>
+          <Button
+            className="theme-btn"
+            variant="danger"
+            onClick={() => setTheme("games")}
+          >
+            Games
+          </Button>
+          <Button
+            className="theme-btn"
+            variant="info"
+            onClick={() => setTheme("sports")}
+          >
+            Sports
+          </Button>
+        </ButtonGroup>
+      </div>
     </div>
   );
 };
