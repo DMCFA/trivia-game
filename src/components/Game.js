415a42989a7ca22f276d0feed92e91973a02a959
Duarte Almeida <duartemalmeida@gmail.com>
2021-10-08 21:40:27 +0100

    add initial useEffect to get questions

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import getData from "../api/questions";
 
-const Game = () => {
+const Game = ({ theme, difficulty, amount }) => {
+  useEffect(() => {
+    const data = getData(theme, difficulty, amount);
+    data.map((el) => console.log(el));
+  }, [theme, amount, difficulty]);
+
   return <div>Hello</div>;
 };
 
