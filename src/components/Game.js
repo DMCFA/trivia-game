4450b3aac604699332952067b9964ed255aed559
Duarte Almeida <duartemalmeida@gmail.com>
2021-10-19 21:14:07 +0100

    disable useEffect function until I am able to pass over state

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,12 +3,18 @@ import axios from "axios";
 import getData from "../api/questions";
 
 const Game = ({ theme, difficulty, amount }) => {
-  useEffect(() => {
-    const data = getData(theme, difficulty, amount);
-    data.map((el) => console.log(el));
-  }, [theme, amount, difficulty]);
+  // useEffect(() => {
+  //   const data = getData(...props);
+  //   data.map((el) => console.log(el));
+  // }, [theme, amount, difficulty]);
 
-  return <div>Hello</div>;
+  return (
+    <div>
+      {console.log(theme)}
+      {console.log(difficulty)}
+      {console.log(amount)}
+    </div>
+  );
 };
 
 export default Game;
