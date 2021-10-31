5cb396af4ccfcbe659ca96225ab3bfb6bb05a303
Duarte Almeida <duartemalmeida@gmail.com>
2021-10-31 11:03:11 +0000

    add game functions component to project

---
+++ src/features/game/gameFunctions.js
@@ -0,0 +1,12 @@
+import react from "react";
+import { Button } from "react-bootstrap";
+
+export const BackToSelectionBtn = () => {
+  return (
+    <div>
+      <Button color="warning" size="lg">
+        Go Back!
+      </Button>
+    </div>
+  );
+};
