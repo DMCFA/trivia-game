ca0b1ca590ade77788cea83d5927a31bf46514be
Duarte Almeida <duartemalmeida@gmail.com>
2021-10-31 11:03:51 +0000

    add 2 useeffect functions and display error message when all fields are not selected

--- src/features/game/Game.js
+++ src/features/game/Game.js
@@ -1,12 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import axios from "axios";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Button } from "react-bootstrap";
 import getData from "../../api/questions";
+import themeChosen from "../../api/questions";
+import { BackToSelectionBtn } from "./gameFunctions";
 
 const Game = () => {
   const theme = useSelector((state) => state.theme.value);
   const difficulty = useSelector((state) => state.difficulty.value);
-  return <div></div>;
+  const amount = useSelector((state) => state.amount.value);
+
+  const [rightAnswer, setRightAnswer] = useState(undefined);
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(1);
+  const [backToSelection, setBackToSelection] = useState(false);
+
+  useEffect(() => {
+    getData(amount, theme, difficulty).then((results) => {
+      setQuestions(results);
+    });
+  }, [amount, theme, difficulty]);
+
+  useEffect(() => {
+    if (questions.results.length === 0) {
+      toast.error("You need to make choices first!");
+      setBackToSelection(true);
+    }
+  }, [questions]);
+
+  return (
+    <div className="game-container">
+      <div className="questions-container">
+        {backToSelection && <BackToSelectionBtn />}
+      </div>
+    </div>
+  );
 };
 
 export default Game;
