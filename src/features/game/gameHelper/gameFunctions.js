3bf5450aca228e287cf5717354d32fa270405f70
Duarte Almeida <duartemalmeida@gmail.com>
2021-11-03 21:35:50 +0000

    create component and add function that redirects user if choices were not made

---
+++ src/features/game/gameHelper/gameFunctions.js
@@ -0,0 +1,11 @@
+import { Button } from "react-bootstrap";
+
+export const BackToSelectionBtn = () => {
+  return (
+    <div>
+      <Button color="warning" size="lg">
+        Go Back!
+      </Button>
+    </div>
+  );
+};
