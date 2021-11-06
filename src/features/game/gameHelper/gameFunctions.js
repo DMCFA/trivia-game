3bc61be0881e9e30bbc0250aed0714c488ad968a
Duarte Almeida <duartemalmeida@gmail.com>
2021-11-06 15:23:41 +0000

    remove back to selection function and add 2 functions, 1 to parse question and the other to shuffle possible answers

--- src/features/game/gameHelper/gameFunctions.js
+++ src/features/game/gameHelper/gameFunctions.js
@@ -1,11 +1,32 @@
-import { Button } from "react-bootstrap";
+////////////*GAME FUNCTIONS*/////////////////
 
-export const BackToSelectionBtn = () => {
-  return (
-    <div>
-      <Button color="warning" size="lg">
-        Go Back!
-      </Button>
-    </div>
-  );
+///*imports*///
+import { toast } from "react-toastify";
+import { hasRequestFailed } from "../../../api/questions";
+
+///*call and parse next question to be displayed*///
+export const nextQuestion = (allQuestions, questionNum) => {
+  const parser = new DOMParser();
+  if (allQuestions.length === 0) {
+    toast.info("Loading...");
+  } else {
+    const question = allQuestions[questionNum].question;
+    const parsedQuestion = parser.parseFromString(question, "text/html").body
+      .textContent;
+    return parsedQuestion;
+  }
+};
+
+///*takes an array with all the possible answers from the api and shuffles them around*///
+export const shuffleAnswers = (answersArray) => {
+  let arrLength = answersArray.length,
+    i,
+    t;
+  while (arrLength) {
+    i = Math.floor(Math.random() * arrLength--);
+    t = answersArray[arrLength];
+    answersArray[t] = answersArray[i];
+    answersArray[i] = t;
+  }
+  return answersArray;
 };
