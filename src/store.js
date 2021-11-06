a4971a66d908ec0f48009aeb4c205c95769a7109
Duarte Almeida <duartemalmeida@gmail.com>
2021-11-06 15:24:09 +0000

    add question reducer to store

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./features/options/theme/themeSlice";
 import difficultyReducer from "./features/options/difficulty/difficultySlice";
 import amountReducer from "./features/options/amount/amountSlice";
+import questionReducer, { questionSlice } from "./features/game/gameSlice";
 
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
     difficulty: difficultyReducer,
     amount: amountReducer,
+    question: questionReducer,
   },
 });
