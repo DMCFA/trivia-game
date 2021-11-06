48e8f9b56052678c307772f6d2af210f021e72a2
Duarte Almeida <duartemalmeida@gmail.com>
2021-11-06 12:24:00 +0000

    import toast css file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Game from "./features/game/Game";
