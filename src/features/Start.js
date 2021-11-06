ae1a07382b7870206e0ef25653205060bcc82093
Duarte Almeida <duartemalmeida@gmail.com>
2021-11-06 15:24:49 +0000

    add function to stop progress if choices are not all made

--- src/features/Start.js
+++ src/features/Start.js
@@ -1,11 +1,36 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import { useHistory } from "react-router";
+import { useSelector } from "react-redux";
+import { ToastContainer, toast } from "react-toastify";
 
 const Start = () => {
   const history = useHistory();
+  const difficulty = useSelector((state) => state.difficulty.value);
+  const theme = useSelector((state) => state.theme.value);
+  const amount = useSelector((state) => state.amount.value);
+
+  //check if all fields have been selected
+  const allFieldsChecked = () => {
+    if (difficulty || theme || amount === "") {
+      toast.error("⚠️ Go back and select all fields!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  };
+
+  //go to game page
   const handleStart = () => {
-    history.push("/game");
+    if (allFieldsChecked()) {
+    } else {
+      history.push("/game");
+    }
   };
   return (
     <div className="start">
@@ -20,6 +45,7 @@ const Start = () => {
           Start
         </Button>
       </div>
+      <ToastContainer />
     </div>
   );
 };
