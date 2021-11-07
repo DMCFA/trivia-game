490cfc49c8709563c0c50e752982b16f0ced25fe
Duarte Almeida <duartemalmeida@gmail.com>
2021-11-07 11:15:47 +0000

    removed state and useEffect actions from the main game component and transfer those to the game questions component. Added a variable with the total score value for future display

--- src/features/game/Game.js
+++ src/features/game/Game.js
@@ -1,38 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { toast } from "react-toastify";
-import { getData, hasRequestFail } from "../../api/questions";
-import GameQuestion from "./gameHelper/GameQuestions";
-import { BackToSelectionBtn } from "./gameHelper/gameFunctions";
+import React from "react";
+import { useSelector } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import GameQuestion from "./gameHelper/gameQuestions";
 
 const Game = () => {
-  const theme = useSelector((state) => state.theme.value);
-  const difficulty = useSelector((state) => state.difficulty.value);
-  const amount = useSelector((state) => state.amount.value);
-
-  const [rightAnswer, setRightAnswer] = useState(undefined);
-  const [questions, setQuestions] = useState([]);
-  const [index, setIndex] = useState(0);
-  const [backToSelection, setBackToSelection] = useState(false);
-
-  useEffect(() => {
-    getData(amount, theme, difficulty).then((results) => {
-      setQuestions(results.results);
-    });
-  }, [amount, theme, difficulty]);
-
-  useEffect(() => {
-    if (hasRequestFail) {
-      toast.error("You need to make choices first!");
-      setBackToSelection(true);
-    }
-  }, []);
+  ///*get final score*\\\
+  const score = useSelector((state) => state.question.value);
 
   return (
     <div>
-      {backToSelection && <BackToSelectionBtn selection={backToSelection} />}
-      <GameQuestion questions={questions} />
+      <GameQuestion />
+      <ToastContainer />
     </div>
   );
 };
