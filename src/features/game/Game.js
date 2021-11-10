4c213190ed2b89c965cd366630c59c619cbd3fbd
Duarte Almeida <duartemalmeida@gmail.com>
2021-11-10 21:43:29 +0000

    add initial useEffect to fetch data in order to conditionally render the pageand add conditional rendering

--- src/features/game/Game.js
+++ src/features/game/Game.js
@@ -1,16 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
+import { getData, hasRequestFailed } from "../../api/questions";
 import GameQuestion from "./gameHelper/GameQuestions";
 import LoadingScreen from "./gameHelper/LoadingScreen";
 
-const Game = ({ ready }) => {
+const Game = () => {
+  const theme = useSelector((state) => state.theme.value);
+  const difficulty = useSelector((state) => state.difficulty.value);
+  const amount = useSelector((state) => state.amount.value);
+  const [questions, setQuestions] = useState([]);
   const score = useSelector((state) => state.question.value);
   const [areQuestionsReady, setAreQuestionsReady] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  ///*display questions*\\\
+  useEffect(() => {
+    getData(amount, theme, difficulty).then((results) => {
+      setQuestions(results.results);
+      setIsLoading(false);
+    });
+    if (hasRequestFailed) {
+      toast.error("❌ Something went wrong! ❌");
+    }
+  }, [amount, theme, difficulty]);
 
   return (
     <div>
-      {ready ? <GameQuestion /> : <LoadingScreen />}
+      {isLoading ? <LoadingScreen /> : <GameQuestion questions={questions} />}
       <ToastContainer />
     </div>
   );
