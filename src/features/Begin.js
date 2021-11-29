289ba2b6476f7d7974deb4daa263372995be4cdc
Duarte Almeida <duartemalmeida@gmail.com>
2021-11-29 21:31:56 +0000

    add dice images to start page and styling

--- src/features/Begin.js
+++ src/features/Begin.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-scroll";
 import Theme from "./options/theme/Theme";
+import dice from "./../img/dice.png";
 
 const Begin = () => {
   return (
     <div className="main">
       <div className="begin">
-        <h1 className="begin-title">🎲 Trivia Game 🎲</h1>
+        <h1 className="begin-title">
+          <img className="dice-img" src={dice} alt="dice" />
+          Trivia Game
+          <img className="dice-img" src={dice} alt="dice" />
+        </h1>
         <div className="begin-btn-group">
           <Link to="theme">
             <Button
