9ea81afdcbd06ef263869ff046af6b72d5e3d921
Duarte Almeida <duartemalmeida@gmail.com>
2021-12-01 22:34:57 +0000

    add boolean function, useeffect for questions, button creation functions and update score function. add conditional rendering to both return divs

--- src/features/game/gameHelper/GameQuestions.js
+++ src/features/game/gameHelper/GameQuestions.js
@@ -1,26 +1,47 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { useDispatch } from "react-redux";
-import { increment } from "./answerSlice";
+import { useHistory } from "react-router";
+import { ButtonGroup, Button } from "react-bootstrap";
+import { increment } from "./../score/scoreSlice";
 
-import {
-  nextQuestion,
-  shuffleAnswers,
-  multipleAnswerButtons,
-  trueOrFalseButtons,
-  storeAnswer,
-} from "./gameFunctions";
+import { nextQuestion, shuffleAnswers } from "./helperFunctions/gameFunctions";
 
 function GameQuestion({ questions }) {
   ///////////////////////////////////
   //local states
   //////////////////////////////////
-  const [displayNextQuestion, setDisplayNextQuestion] = useState(true);
-  const [isBoolean, setIsBoolean] = useState(false);
   const [answerOptions, setAnswerOptions] = useState([]);
   const [questionNum, setQuestionNum] = useState(0);
 
+  ///////////////////////////////////
+  //set boolean value and state
+  //////////////////////////////////
+  const useToggle = (initialValue = false) => {
+    const [value, setValue] = useState(initialValue);
+    const toggle = useCallback(() => {
+      setValue((v) => !v);
+    }, []);
+    return [value, toggle];
+  };
+
+  const [isBoolean, toggleIsOn] = useToggle();
+
   const dispatch = useDispatch();
+  const history = useHistory();
+
   const displayQuestion = nextQuestion(questions, questionNum);
+
+  ///////////////////////////////////
+  //set answers
+  //////////////////////////////////
+  useEffect(() => {
+    (async () => {
+      const answers = await hasMultipleAnswers(questions);
+      if (!isBoolean) {
+        setAnswerOptions(answers);
+      }
+    })();
+  }, [questionNum]);
 
   ///////////////////////////////////
   //check if question type is multiple or boolean
@@ -33,52 +54,116 @@ function GameQuestion({ questions }) {
       shuffleAnswers(allAnswers);
       return allAnswers;
     }
-    setIsBoolean(true);
+    toggleIsOn();
   };
 
-  ///////////////////////////////////
-  //set answers
-  //////////////////////////////////
-  useEffect(() => {
-    const answers = hasMultipleAnswers(questions);
-    setAnswerOptions(answers);
-  }, [questions]);
+  ///* create buttons for multiple answers* \\\
+  const multipleAnswerButtons = (answerOptions) => {
+    return (
+      <ButtonGroup aria-label="group of buttons" size="lg">
+        <Button
+          className="answers-btn"
+          variant="dark"
+          value={answerOptions[0]}
+          onClick={(e) => processAnswer(e)}
+        >
+          {answerOptions[0]}
+        </Button>
+        <Button
+          className="answers-btn"
+          variant="dark"
+          value={answerOptions[1]}
+          onClick={(e) => processAnswer(e)}
+        >
+          {answerOptions[1]}
+        </Button>
+        <Button
+          className="answers-btn"
+          variant="dark"
+          value={answerOptions[2]}
+          onClick={(e) => processAnswer(e)}
+        >
+          {answerOptions[2]}
+        </Button>
+        <Button
+          className="answers-btn"
+          variant="dark"
+          value={answerOptions[3]}
+          onClick={(e) => processAnswer(e)}
+        >
+          {answerOptions[3]}
+        </Button>
+      </ButtonGroup>
+    );
+  };
 
-  ///////////////////////////////////
-  //display buttons
-  //////////////////////////////////
-  const createButtons = (answers) => {
-    if (!isBoolean) {
-      return multipleAnswerButtons(answers);
-    }
-    return trueOrFalseButtons();
+  ///* create buttons for true or false answers* \\\
+  const trueOrFalseButtons = () => {
+    return (
+      <ButtonGroup aria-label="group of buttons" size="lg">
+        <Button
+          className="answers-btn-boolean"
+          variant="success"
+          value="True"
+          onClick={(e) => {
+            processAnswer(e);
+          }}
+        >
+          True
+        </Button>
+        <Button
+          className="answers-btn-boolean"
+          variant="danger"
+          value="False"
+          onClick={(e) => processAnswer(e)}
+        >
+          False
+        </Button>
+      </ButtonGroup>
+    );
   };
 
   ///////////////////////////////////
   //update score
   //////////////////////////////////
-  const updateScore = () => {
+  const updateScore = (answer) => {
+    const currentAnswer = answer;
     const question = questions[questionNum];
-    if (storeAnswer === question.correct_answer) {
+    if (currentAnswer === question.correct_answer) {
       dispatch(increment());
     }
   };
 
   ///////////////////////////////////
-  //confirm if there are any more questions
+  //answer button clicked and next question
   //////////////////////////////////
-  const anyQuestionsLeft = () => questionNum < questions.length - 1;
+  const processAnswer = (e) => {
+    e.preventDefault();
+    const answer = e.target.value;
+    updateScore(answer);
+    setQuestionNum(questionNum + 1);
+    if (!anyMoreQuestions) {
+      history.push("/final");
+    }
+  };
 
   ///////////////////////////////////
-  //display next question
+  //check if there are any more questions
   //////////////////////////////////
+  const anyMoreQuestions = () => {
+    return questionNum < questions.length - 1;
+  };
 
   return (
     <div className="game-container">
       <div className="questions-container">
         <div className="question">
-          <p>{displayQuestion}</p>
-          <div>{createButtons(answerOptions)}</div>
+          <p>{anyMoreQuestions && displayQuestion}</p>
+          <div>
+            {isBoolean
+              ? trueOrFalseButtons()
+              : multipleAnswerButtons(answerOptions)}
+          </div>
         </div>
       </div>
     </div>
