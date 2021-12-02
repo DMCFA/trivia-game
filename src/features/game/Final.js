be3ccc123d9ad3afc3f8f78972e5a8b265522b2e
Duarte Almeida <duartemalmeida@gmail.com>
2021-12-02 08:55:52 +0000

    finish final page look and logic

--- src/features/game/Final.js
+++ src/features/game/Final.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router";
+import { Button } from "react-bootstrap";
 
 const Final = () => {
+  const score = useSelector((state) => state.question.value);
+  const history = useHistory();
+
+  const handleClick = () => history.push("/");
   return (
-    <div>
-      <h1>Final Score</h1>
+    <div className="final-container">
+      <h1 className="final-screen">You got {score} answers right!</h1>
+      <Button
+        type="button"
+        className="start-over btn btn-light"
+        onClick={handleClick}
+      >
+        Start Over?
+      </Button>
     </div>
   );
 };
